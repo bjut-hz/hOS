cfad2148d52d6aa07dc9369cfd84abb63a8410f3
何震震 <936432896@qq.com>
2015-08-25 17:21:15 +0800

    enter the protect mode and work in it
    1. pm.inc : the definition of the data structure and constants
    2. test.asm: it shows that how can we enter the protect mode.
    3. With the help of freedos, we can observe the result of our program
    easily.

---
+++ Protect Mode/test1/test.asm
@@ -0,0 +1,78 @@
+%include 	"pm.inc"
+
+org	0100h
+	jmp	LABEL_BEGIN
+
+[SECTION .gdt]
+LABEL_GDT:	   Descriptor       0,                0, 0           
+LABEL_DESC_CODE32: Descriptor       0, SegCode32Len - 1, DA_C + DA_32
+LABEL_DESC_VIDEO:  Descriptor 0B8000h,           0ffffh, DA_DRW	     
+
+GdtLen		equ	$ - LABEL_GDT
+GdtPtr		dw	GdtLen - 1
+		dd	0
+
+SelectorCode32		equ	LABEL_DESC_CODE32	- LABEL_GDT
+SelectorVideo		equ	LABEL_DESC_VIDEO	- LABEL_GDT
+
+[SECTION .s16]
+[BITS	16]
+LABEL_BEGIN:
+	mov	ax, cs
+	mov	ds, ax
+	mov	es, ax
+	mov	ss, ax
+	mov	sp, 0100h
+
+	; 初始化 32 位代码段描述符
+	xor	eax, eax
+	mov	ax, cs
+	shl	eax, 4
+	add	eax, LABEL_SEG_CODE32
+	mov	word [LABEL_DESC_CODE32 + 2], ax
+	shr	eax, 16
+	mov	byte [LABEL_DESC_CODE32 + 4], al
+	mov	byte [LABEL_DESC_CODE32 + 7], ah
+
+	; 为加载 GDTR 作准备
+	xor	eax, eax
+	mov	ax, ds
+	shl	eax, 4
+	add	eax, LABEL_GDT
+	mov	dword [GdtPtr + 2], eax
+
+	lgdt	[GdtPtr]
+
+	cli
+
+	; 打开地址线A20
+	in	al, 92h
+	or	al, 00000010b
+	out	92h, al
+
+	; 准备切换到保护模式
+	mov	eax, cr0
+	or	eax, 1
+	mov	cr0, eax
+
+	; 真正进入保护模式
+	jmp	dword SelectorCode32:0
+
+
+[SECTION .s32]; 32 位代码段. 由实模式跳入.
+[BITS	32]
+
+LABEL_SEG_CODE32:
+	mov	ax, SelectorVideo
+	mov	gs, ax			; 视频段选择子(目的)
+
+	mov	edi, (80 * 11 + 79) * 2	; 屏幕第 11 行, 第 79 列。
+	mov	ah, 0Ch			; 0000: 黑底    1100: 红字
+	mov	al, 'P'
+	mov	[gs:edi], ax
+
+	; 到此停止
+	jmp	$
+
+SegCode32Len	equ	$ - LABEL_SEG_CODE32
+
